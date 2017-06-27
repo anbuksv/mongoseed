38933eeecfb1eeb1da936fcdb009f932505ec3ae
Anbu <anbu@megtag.com>
2017-06-27 12:40:37 +0530

    query based on appId added

--- amongo.bat
+++ amongo.bat
@@ -3,8 +3,9 @@ echo The app id is : %1
 rem echo "Are you sure do you want execute the operation ? (Y|N) "
 rem pause
 rem %1 appId %2 host %3 port %4 db %5 collection
-echo mongoexport --host %2 --port %3 --db %4 --collection %5 --jsonArray --out "./mongo/intentResponses.json"
-mongoexport --host %2 --port %3 --db %4 --collection %5 --jsonArray --out "./mongo/intentResponses.json"
+mongoexport --host %2 --port %3 --db %4 --collection %5 --jsonArray --out "./mongo/intentResponsesBackup.json"
+rem echo mongoexport --host %2 --port %3 --db %4 --collection %5 --jsonArray --query %6 --out "./mongo/intentResponses.json"
+mongoexport --host %2 --port %3 --db %4 --collection %5 --jsonArray --query %6 --out "./mongo/intentResponses.json"
 node mongo.js %1
-echo mongoimport --host %2 --port %3 --db %4 --collection %5 --jsonArray --file "./mongo/intentResponses.json"
+rem echo mongoimport --host %2 --port %3 --db %4 --collection %5 --jsonArray --file "./mongo/intentResponses.json"
 mongoimport --host %2 --port %3 --db %4 --collection %5 --jsonArray --file "./mongo/intentResponses.json"
--- mongorestore.bat
+++ mongorestore.bat
@@ -1,3 +1,3 @@
 @echo off
-mongoimport --host %1 --port %2 --db %3 --collection %4 --drop --jsonArray --file "./mongo/intentResponses_Backup.json"
+mongoimport --host %1 --port %2 --db %3 --collection %4 --drop --jsonArray --file "./mongo/intentResponsesBackup.json"
 echo "collection successfully restored"
